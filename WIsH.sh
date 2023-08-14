41d1548259ddb1bb69be8f61a2aace4a2af8f851
Abigail Howell <aahowel3@asu.edu>
2023-08-14 13:34:18 -0700

    Delete WIsH.sh

--- WIsH.sh
+++
@@ -1,6 +0,0 @@
-#create host model directory 
-WIsH/WIsH -c build -g phage_hosts_genomes/ -m modelDir 
-#Run predict on null to get llikelihood.matrix in outputNullModelResultDir to feed into computeNullParameters.R 
-WIsH/WIsH -c predict -g null/ -m modelDir -r outputNullModelResultDir -b 1000  
-Rscript ../WIsH/computeNullParameters.R  
-WIsH/WIsH -c predict -g phage_genomes/ -m modelDir -r outputResultDir -b 1000 -n outputNullModelResultDir/nullParameters.tsv 
