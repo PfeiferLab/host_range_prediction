0bdb0a7177534bf4be06ca4410ac305dcbd4f5a1
Abigail Howell <aahowel3@asu.edu>
2023-08-14 13:34:58 -0700

    Delete PHP.sh

--- PHP.sh
+++
@@ -1,2 +0,0 @@
-python3 PHP/countKmer.py -f phage_host_genomes -d phage_host_PHPkmer -n phage_PHPHostKmer -c -1 
-python3 PHP/PHP.py -v phage_genomes -o phage_PHPout -d phage_PHPkmer -n phage_PHPHostKmer
