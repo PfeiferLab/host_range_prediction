6c212354372a93bdba1d6f36b6ceac44c9ea3036
Abigail Howell <aahowel3@asu.edu>
2023-09-05 09:44:02 -0700

    Update dotplots_ani_combined_formanuscript.R

--- dotplots_ani_combined_formanuscript.R
+++ dotplots_ani_combined_formanuscript.R
@@ -1,8 +1,5 @@
-################# use this
 library(dplyr)
-###susanne wants you to try and combine and rescale
 library(data.table)
-# Load libraries.
 library(reshape)
 library(ggplot2)
 library(viridis)
@@ -11,36 +8,33 @@ library(scales)
 library(dplyr)
 library(RColorBrewer)
 
+#all gordonia and e. coli phage datasets combined
+combined=read.csv("everything_all.csv")
 
-###Import files in most base form to do only minor edits then feed in here - change only directory 
-#and chamge title at the bottom 
-setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/WISH/formanuscript/SFP10/")
-combined=read.csv("Gordoniaall_confirmatory.csv")
 #read in all columns
 colnames(combined) <- c("X","variable","value","shape","shape2","tool","pvalue")
 #but only select these, leaveoutwish plvaues intil upset plot
-combined=combined[,c("X","variable","value","shape","shape2","tool")]
+#combined=combined[,c("X","variable","value","shape","shape2","tool")]
+#combined <- ddply(combined, .(X), transform, rescale = rescale(value))
+#combined <- ddply(combined, .(variable), transform, rescale2 = rescale(value))
 
+combined$finalvalue=ifelse(combined$tool=="WIsH", combined$pvalue, combined$value)
+combined$finalvalue2 = ifelse(combined$tool=="VHM" | combined$tool=="WIsH", 1-combined$finalvalue,combined$finalvalue)
 
-combined <- ddply(combined, .(X), transform, rescale = rescale(value))
-combined <- ddply(combined, .(variable), transform, rescale2 = rescale(value))
-combined <- ddply(combined, .(tool), transform, rescale3 = rescale(value))
-
+#rescale per tool across all datasets
+combined <- ddply(combined, .(tool), transform, rescale3 = rescale(finalvalue2))
 
 combined$variable=sub("^(\\S+) (\\S+) ", "\\1 \\2)~(", combined$variable)
 combined$variable=paste0("italic(", combined$variable)
 combined$variable=sub(" ", "", combined$variable)
 combined$variable <- paste0(combined$variable, ")")
 
+table(combined$tool)
 
 #groups x axis by tool
 combined$rename <- paste(combined$tool,combined$X)
-#groups x axis by phage
-#combined$rename <- paste(combined$X,combined$tool)
 
-
-#combined <- combined[order(combined$X),]
-
+GMAs=combined[combined$X %like% "G",]
 labelsf=c("GMA2","GMA3","GMA4","GMA5","GMA6","GMA7","GRU1","GRU3","GTE2","GTE5","GTE6","GTE7","GTE8",
           "GMA2","GMA3","GMA4","GMA5","GMA6","GMA7","GRU1","GRU3","GTE2","GTE5","GTE6","GTE7","GTE8",
           "GMA2","GMA3","GMA4","GMA5","GMA6","GMA7","GRU1","GRU3","GTE2","GTE5","GTE6","GTE7","GTE8",
@@ -51,11 +45,11 @@ library(forcats)
 library(ggplot2)
 # Plot.
 q = ggplot() +
-  geom_point(data=combined, aes(rename, fct_inorder(variable), color=rescale3, shape = shape), size = 5) +
+  geom_point(data=GMAs, aes(rename, fct_inorder(variable), color=rescale3, shape = shape), size = 5) +
   #scale_shape_identity() +
   #scale_color_viridis(option = "viridis", name = "log likelihood") +
   scale_x_discrete(labels = labelsf) + 
-  scale_y_discrete(labels = parse(text = unique(combined$variable))) + 
+  scale_y_discrete(labels = parse(text = unique(GMAs$variable))) + 
   #keep sizes 18 and 14 for GMA/GRU graphs 
   theme(axis.text = element_text(face="bold"), # make the axis labels bold to make it more readable
         axis.text.x = element_text(angle = 45, hjust = 1, size = 10), # make the x-axis labels vertical for readability
@@ -77,12 +71,12 @@ q = ggplot() +
                             name = "experimentally validated host?") + 
   
   ###this whole next part is just to get the border
-  geom_point(data=combined, aes(rename, fct_inorder(variable), shape = shape2), size = 5, stroke=2) +
+  geom_point(data=GMAs, aes(rename, fct_inorder(variable), shape = shape2), size = 5, stroke=2) +
   
   #scale_shape_identity() +
   #scale_color_viridis(option = "viridis", name = "log likelihood") +
   scale_x_discrete(labels = labelsf) + 
-  scale_y_discrete(labels = parse(text = unique(combined$variable))) +
+  scale_y_discrete(labels = parse(text = unique(GMAs$variable))) +
   
   theme(axis.text = element_text(face="bold"), # make the axis labels bold to make it more readable
         axis.text.x = element_text(angle = 45, hjust = 1), # make the x-axis labels vertical for readability
@@ -106,46 +100,19 @@ q
 
 ################################################################
 #############for ecoli viruses
-###########################################
-###Import files in most base form to do only minor edits then feed in here - change only directory 
-#and chamge title at the bottom 
-setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/WISH/formanuscript/KFS-EC3/")
-combined=read.csv("KFS-EC3_confirmatory.csv")
-
-setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/WISH/formanuscript/HY01/")
-combined=read.csv("HY01_confirmatory.csv")
-
-setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/WISH/formanuscript/SFP10/")
-combined=read.csv("SFP10_Onlyconfirmatory.csv")
-
-
-#read in all columns
-colnames(combined) <- c("X","variable","value","shape","shape2","tool","pvalue")
-#but only select these, leaveoutwish plvaues intil upset plot
-combined=combined[,c("X","variable","value","shape","shape2","tool")]
-
-
-#combined <- ddply(combined, .(X), transform, rescale = rescale(value))
-#combined <- ddply(combined, .(variable), transform, rescale2 = rescale(value))
-#rescale by tool
-combined <- ddply(combined, .(tool), transform, rescale3 = rescale(value))
-
-
-combined$variable=sub("^(\\S+) (\\S+) ", "\\1 \\2)~(", combined$variable)
-combined$variable=paste0("italic(", combined$variable)
-combined$variable=sub(" ", "", combined$variable)
-combined$variable <- paste0(combined$variable, ")")
-
+ecoli=combined[combined$X == "KFS-EC3",]
+ecoli=combined[combined$X == "HY01",]
+ecoli=combined[combined$X == "SFP10",]
 
 
 library(forcats)
 library(ggplot2)
 # Plot.
 q = ggplot() +
-  geom_point(data=combined, aes(tool, fct_inorder(variable), color=rescale3, shape = shape), size = 12) +
+  geom_point(data=ecoli, aes(tool, fct_inorder(variable), color=rescale3, shape = shape), size = 12) +
   #scale_shape_identity() +
   #scale_color_viridis(option = "viridis", name = "log likelihood") +
-  scale_y_discrete(labels = parse(text = unique(combined$variable))) + 
+  scale_y_discrete(labels = parse(text = unique(ecoli$variable))) + 
   #keep sizes 18 and 14 for GMA/GRU graphs 
   theme(axis.text = element_text(face="bold"), # make the axis labels bold to make it more readable
         axis.text.x = element_text(angle = 45, hjust = 1, size = 15), # make the x-axis labels vertical for readability
@@ -167,11 +134,11 @@ q = ggplot() +
                             name = "experimentally validated host?") + 
   
   ###this whole next part is just to get the border
-  geom_point(data=combined, aes(tool, fct_inorder(variable), shape = shape2), size = 11, stroke=2) +
+  geom_point(data=ecoli, aes(tool, fct_inorder(variable), shape = shape2), size = 11, stroke=2) +
   
   #scale_shape_identity() +
   #scale_color_viridis(option = "viridis", name = "log likelihood") +
-  scale_y_discrete(labels = parse(text = unique(combined$variable))) +
+  scale_y_discrete(labels = parse(text = unique(ecoli$variable))) +
   
   theme(axis.text = element_text(face="bold"), # make the axis labels bold to make it more readable
         axis.text.x = element_text(angle = 45, hjust = 1), # make the x-axis labels vertical for readability
@@ -188,7 +155,4 @@ q = ggplot() +
                             name = "experimentally validated host?") 
 
 q
-#maybe put title on hold
-#q + ggtitle("SFP10") +
-#  theme(plot.title = element_text(size = 22, hjust = 0.5))
 
